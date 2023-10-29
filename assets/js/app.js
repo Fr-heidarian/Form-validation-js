5e6a782dba38c50eba2df031ddc549a23d53c0ae
Farideh <heidarian89@gmail.com>
2023-10-29 00:31:56 -0700

    add validation for email input

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,15 @@
 
 document.querySelector("#btn-submit").addEventListener("click", () => {
   const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
   if (name.value == "") {
     document.getElementById("name-error").innerHTML = "name is required";
-    document.getElementById("name-error").classList.add("name-error")
+    document.getElementById("name-error").classList.add("error")
     // document.getElementById("name-error").style.cssText =
     //   "border: 1px solid #58151c !important; padding:0.25rem 1rem !important;";
   }
+   if (email.value == "") {
+     document.getElementById("email-error").innerHTML = "email is required";
+     document.getElementById("email-error").classList.add("error");
+   }
 });
